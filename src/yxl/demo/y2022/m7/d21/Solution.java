bd999928af5dd1ad324a0fb7f4655da2b57c9ef0
余鑫磊 <1627419257@qq.com>
2022-07-21 09:56:11 +0800

    leetcode 2022.7.21

---
+++ src/yxl/demo/y2022/m7/d21/Solution.java
@@ -0,0 +1,39 @@
+package yxl.demo.y2022.m7.d21;
+
+/**
+ * @Author: yxl
+ * @Date: 2022/7/21 9:48
+ */
+public class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        if (root.left == null && root.right == null && root.val == 0) {
+            return null;
+        }
+        return root;
+    }
+
+}
