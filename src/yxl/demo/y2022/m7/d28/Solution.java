dd41bfd4f510aa39266be187498d0eb043a15661
余鑫磊 <1627419257@qq.com>
2022-07-28 10:27:13 +0800

    leetcode 2022.7.28

--- src/yxl/demo/y2022/m7/d28/Solution.java
+++ src/yxl/demo/y2022/m7/d28/Solution.java
@@ -1,5 +1,25 @@
-package yxl.demo.y2022.m7.d28;/**
-* @Author: yxl
-* @Date: 2022/7/28 10:18
-*/public class Solution {
+package yxl.demo.y2022.m7.d28;
+
+import java.util.*;
+
+/**
+ * @Author: yxl
+ * @Date: 2022/7/28 10:18
+ */
+public class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int[] sort = arr.clone();
+        Arrays.sort(sort);
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = arr.length, idx = 0;
+
+        for (int i : sort) {
+            if (!map.containsKey(i)) map.put(i, ++idx);
+        }
+
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++)
+            result[i] = map.get(arr[i]);
+        return result;
+    }
 }
