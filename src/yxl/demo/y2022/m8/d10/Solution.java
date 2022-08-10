3b6e44c689c91f3885e2bd8e835d33d6111dc026
余鑫磊 <1627419257@qq.com>
2022-08-10 10:15:53 +0800

    leetcode 2022.8.10

---
+++ src/yxl/demo/y2022/m8/d10/Solution.java
@@ -0,0 +1,43 @@
+package yxl.demo.y2022.m8.d10;
+
+/**
+ * @Author: yxl
+ * @Date: 2022/8/10 10:14
+ */
+public class Solution {
+    public String solveEquation(String equation) {
+        int factor = 0, val = 0;
+        int index = 0, n = equation.length(), sign1 = 1; // 等式左边默认系数为正
+        while (index < n) {
+            if (equation.charAt(index) == '=') {
+                sign1 = -1; // 等式右边默认系数为负
+                index++;
+                continue;
+            }
+
+            int sign2 = sign1, number = 0;
+            boolean valid = false; // 记录 number 是否有效
+            if (equation.charAt(index) == '-' || equation.charAt(index) == '+') { // 去掉前面的符号
+                sign2 = (equation.charAt(index) == '-') ? -sign1 : sign1;
+                index++;
+            }
+            while (index < n && Character.isDigit(equation.charAt(index))) {
+                number = number * 10 + (equation.charAt(index) - '0');
+                index++;
+                valid = true;
+            }
+
+            if (index < n && equation.charAt(index) == 'x') { // 变量
+                factor += valid ? sign2 * number : sign2;
+                index++;
+            } else { // 数值
+                val += sign2 * number;
+            }
+        }
+
+        if (factor == 0) {
+            return val == 0 ? "Infinite solutions" : "No solution";
+        }
+        return "x=" + (-val / factor);
+    }
+}
