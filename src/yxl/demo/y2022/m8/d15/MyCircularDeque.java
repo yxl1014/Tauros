c54c2925a0e748b748ddee35e5ac985b15b69d2e
余鑫磊 <1627419257@qq.com>
2022-08-15 09:23:18 +0800

    leetcode 2022.8.15

---
+++ src/yxl/demo/y2022/m8/d15/MyCircularDeque.java
@@ -0,0 +1,73 @@
+package yxl.demo.y2022.m8.d15;
+
+/**
+ * @Author: yxl
+ * @Date: 2022/8/15 9:22
+ */
+public class MyCircularDeque {
+    private int[] elements;
+    private int rear, front;
+    private int capacity;
+
+    public MyCircularDeque(int k) {
+        capacity = k + 1;
+        rear = front = 0;
+        elements = new int[k + 1];
+    }
+
+    public boolean insertFront(int value) {
+        if (isFull()) {
+            return false;
+        }
+        front = (front - 1 + capacity) % capacity;
+        elements[front] = value;
+        return true;
+    }
+
+    public boolean insertLast(int value) {
+        if (isFull()) {
+            return false;
+        }
+        elements[rear] = value;
+        rear = (rear + 1) % capacity;
+        return true;
+    }
+
+    public boolean deleteFront() {
+        if (isEmpty()) {
+            return false;
+        }
+        front = (front + 1) % capacity;
+        return true;
+    }
+
+    public boolean deleteLast() {
+        if (isEmpty()) {
+            return false;
+        }
+        rear = (rear - 1 + capacity) % capacity;
+        return true;
+    }
+
+    public int getFront() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return elements[front];
+    }
+
+    public int getRear() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return elements[(rear - 1 + capacity) % capacity];
+    }
+
+    public boolean isEmpty() {
+        return rear == front;
+    }
+
+    public boolean isFull() {
+        return (rear + 1) % capacity == front;
+    }
+}
