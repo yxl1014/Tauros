2d9cc7cc16c430ae0490c9884421eb6811e1eb76
yxl1014 <1627419257@qq.com>
2022-08-30 08:57:07 +0800

    leetcode 2022.8.30

---
+++ src/yxl/demo/y2022/m8/d30/Solution.java
@@ -0,0 +1,49 @@
+package yxl.demo.y2022.m8.d30;
+
+/**
+ * User:  yxl
+ * Date:  2022/8/30
+ *
+ * @author yxl
+ */
+
+public class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        TreeNode parent = null;
+        TreeNode cur = root;
+        while (cur != null) {
+            if (val > cur.val) {
+                if (parent == null) {
+                    return new TreeNode(val, root, null);
+                }
+                TreeNode node = new TreeNode(val, cur, null);
+                parent.right = node;
+                return root;
+            } else {
+                parent = cur;
+                cur = cur.right;
+            }
+        }
+        parent.right = new TreeNode(val);
+        return root;
+    }
+}
