e13b510ddb19479b4191913b5456848ac4a1da6b
yxl <1627419257@qq.com>
2022-08-31 21:15:25 +0800

    leetcode 2022.8.31

---
+++ src/yxl/demo/y2022/m8/d31/Solution.java
@@ -0,0 +1,19 @@
+package yxl.demo.y2022.m8.d31;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Deque<Integer> stack = new ArrayDeque<Integer>();
+        int n = pushed.length;
+        for (int i = 0, j = 0; i < n; i++) {
+            stack.push(pushed[i]);
+            while (!stack.isEmpty() && stack.peek() == popped[j]) {
+                stack.pop();
+                j++;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
