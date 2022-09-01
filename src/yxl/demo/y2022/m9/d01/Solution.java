58f4ea928f3fe8f121389d84a17d07b6347dfef1
yxl <1627419257@qq.com>
2022-09-01 20:05:22 +0800

    leetcode 2022.9.1

---
+++ src/yxl/demo/y2022/m9/d01/Solution.java
@@ -0,0 +1,19 @@
+package yxl.demo.y2022.m9.d01;
+
+public class Solution {
+    public int[] finalPrices(int[] prices) {
+        int n = prices.length;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; ++i) {
+            int discount = 0;
+            for (int j = i + 1; j < n; ++j) {
+                if(prices[j] <= prices[i]){
+                    discount = prices[j];
+                    break;
+                }
+            }
+            ans[i] = prices[i] - discount;
+        }
+        return ans;
+    }
+}
