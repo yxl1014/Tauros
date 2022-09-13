fc00c22c859da5ea271acf489acb6f2eafabd387
yxl1014 <1627419257@qq.com>
2022-09-13 15:33:02 +0800

    leetcode 2022.9.13

---
+++ src/yxl/demo/y2022/m9/d13/Solution.java
@@ -0,0 +1,27 @@
+package yxl.demo.y2022.m9.d13;
+
+/**
+ * @author yxl
+ * @date: 2022/9/13 下午3:32
+ */
+public class Solution {
+    public int maximumSwap(int num) {
+        char[] charArray = String.valueOf(num).toCharArray();
+        int n = charArray.length;
+        int maxNum = num;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                swap(charArray, i, j);
+                maxNum = Math.max(maxNum, Integer.parseInt(new String(charArray)));
+                swap(charArray, i, j);
+            }
+        }
+        return maxNum;
+    }
+
+    public void swap(char[] charArray, int i, int j) {
+        char temp = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+    }
+}
