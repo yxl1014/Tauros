076a87bf33e0e82c8d4c0089fb420cc4683dd289
yxl1014 <1627419257@qq.com>
2022-09-20 09:41:19 +0800

    leetcode 2022.9.20

---
+++ src/yxl/demo/y2022/m9/d20/Solution.java
@@ -0,0 +1,43 @@
+package yxl.demo.y2022.m9.d20;
+
+import java.util.Arrays;
+
+/**
+ * @author yxl
+ * @date: 2022/9/20 上午9:35
+ */
+public class Solution {
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        int all = Arrays.stream(nums).sum();
+        if (all % k != 0) {
+            return false;
+        }
+        int per = all / k;
+        Arrays.sort(nums);
+        int n = nums.length;
+        if (nums[n - 1] > per) {
+            return false;
+        }
+        boolean[] dp = new boolean[1 << n];
+        int[] curSum = new int[1 << n];
+        dp[0] = true;
+        for (int i = 0; i < 1 << n; i++) {
+            if (!dp[i]) {
+                continue;
+            }
+            for (int j = 0; j < n; j++) {
+                if (curSum[i] + nums[j] > per) {
+                    break;
+                }
+                if (((i >> j) & 1) == 0) {
+                    int next = i | (1 << j);
+                    if (!dp[next]) {
+                        curSum[next] = (curSum[i] + nums[j]) % per;
+                        dp[next] = true;
+                    }
+                }
+            }
+        }
+        return dp[(1 << n) - 1];
+    }
+}
