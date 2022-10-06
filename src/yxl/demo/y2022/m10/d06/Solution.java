b004d5484b9375f06566999d6525c714baf34a48
yxl1014 <1627419257@qq.com>
2022-10-06 13:41:41 +0800

    leetcode 2022.10.6

---
+++ src/yxl/demo/y2022/m10/d06/Solution.java
@@ -0,0 +1,52 @@
+package yxl.demo.y2022.m10.d06;
+
+import java.util.Arrays;
+
+/**
+ * @author yxl
+ * @date: 2022/10/6 下午1:40
+ *
+作者：LeetCode-Solution
+链接：https://leetcode.cn/problems/three-equal-parts/solution/san-deng-fen-by-leetcode-solution-3l2y/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+public class Solution {
+    public int[] threeEqualParts(int[] arr) {
+        int sum = Arrays.stream(arr).sum();
+        if (sum % 3 != 0) {
+            return new int[]{-1, -1};
+        }
+        if (sum == 0) {
+            return new int[]{0, 2};
+        }
+
+        int partial = sum / 3;
+        int first = 0, second = 0, third = 0, cur = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                if (cur == 0) {
+                    first = i;
+                } else if (cur == partial) {
+                    second = i;
+                } else if (cur == 2 * partial) {
+                    third = i;
+                }
+                cur++;
+            }
+        }
+
+        int len = arr.length - third;
+        if (first + len <= second && second + len <= third) {
+            int i = 0;
+            while (third + i < arr.length) {
+                if (arr[first + i] != arr[second + i] || arr[first + i] != arr[third + i]) {
+                    return new int[]{-1, -1};
+                }
+                i++;
+            }
+            return new int[]{first + len - 1, second + len};
+        }
+        return new int[]{-1, -1};
+    }
+}
