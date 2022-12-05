2a1a05242ad78f3b8ea1d511ba5712b342500d00
yxl <1627419257@qq.com>
2022-12-05 15:06:50 +0800

    leetcode 2022.12.05

--- src/yxl/demo/y2022/m12/d04/Solution.java
+++ src/yxl/demo/y2022/m12/d04/Solution.java
@@ -11,19 +11,19 @@ public class Solution {
      * 来源：力扣（LeetCode）
      * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
      */
-    public int closestCost(int[] base, int[] top, int target) {
+    public int closestCost(int[] baseCosts, int[] toppingCosts, int target) {
         boolean[] dp = new boolean[target + 5];
         //这个参数是大于target的最小组合
         int ans = Integer.MAX_VALUE;
         //因为必须选一个基料，且不用加配料 所以若基料小于target将数组相同下标设置成true
-        for (int x : base) { //判断只能选
+        for (int x : baseCosts) { //判断只能选
             //如果大于target 与ans去最小值
             if (x > target) ans = Math.min(ans, x);
             else dp[x] = true;
         }
 
         //便利每种配料
-        for (int x : top) {
+        for (int x : toppingCosts) {
             //选2次
             for (int i = 0; i < 2; i++) {
                 for (int j = target; j >= 0; j--) {
---
+++ src/yxl/demo/y2022/m12/d05/Solution.java
@@ -0,0 +1,55 @@
+package yxl.demo.y2022.m12.d05;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author yxl
+ * @date 2022/12/5 下午3:05
+ */
+public class Solution {
+    /**
+     * 作者：力扣官方题解
+     * 链接：https://leetcode.cn/problems/delivering-boxes-from-storage-to-ports/solutions/2005717/cong-cang-ku-dao-ma-tou-yun-shu-xiang-zi-4uya/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    public int boxDelivering(int[][] boxes, int portsCount, int maxBoxes, int maxWeight) {
+        int n = boxes.length;
+        int[] p = new int[n + 1];
+        int[] w = new int[n + 1];
+        int[] neg = new int[n + 1];
+        long[] W = new long[n + 1];
+        for (int i = 1; i <= n; ++i) {
+            p[i] = boxes[i - 1][0];
+            w[i] = boxes[i - 1][1];
+            if (i > 1) {
+                neg[i] = neg[i - 1] + (p[i - 1] != p[i] ? 1 : 0);
+            }
+            W[i] = W[i - 1] + w[i];
+        }
+
+        Deque<Integer> opt = new ArrayDeque<Integer>();
+        opt.offerLast(0);
+        int[] f = new int[n + 1];
+        int[] g = new int[n + 1];
+
+        for (int i = 1; i <= n; ++i) {
+            while (i - opt.peekFirst() > maxBoxes || W[i] - W[opt.peekFirst()] > maxWeight) {
+                opt.pollFirst();
+            }
+
+            f[i] = g[opt.peekFirst()] + neg[i] + 2;
+
+            if (i != n) {
+                g[i] = f[i] - neg[i + 1];
+                while (!opt.isEmpty() && g[i] <= g[opt.peekLast()]) {
+                    opt.pollLast();
+                }
+                opt.offerLast(i);
+            }
+        }
+
+        return f[n];
+    }
+}
