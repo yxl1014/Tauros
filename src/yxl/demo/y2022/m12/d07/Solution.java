52d220229b3b3be71d8d14573f71c90f4e22a04b
yxl <1627419257@qq.com>
2022-12-07 18:57:38 +0800

    leetcode 2022.12.07

---
+++ src/yxl/demo/y2022/m12/d07/Solution.java
@@ -0,0 +1,42 @@
+package yxl.demo.y2022.m12.d07;
+
+import java.util.Arrays;
+
+/**
+ * @author yxl
+ * @date 2022/12/7 下午6:51
+ */
+public class Solution {
+    /**
+     * 作者：灵茶山艾府
+     * 链接：https://leetcode.cn/problems/equal-sum-arrays-with-minimum-number-of-operations/solutions/2009786/mei-xiang-ming-bai-yi-ge-dong-hua-miao-d-ocuu/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    public int minOperations(int[] nums1, int[] nums2) {
+        if (6 * nums1.length < nums2.length || 6 * nums2.length < nums1.length)
+            return -1; // 优化
+        // int d = Arrays.stream(nums2).sum() - Arrays.stream(nums1).sum();
+        int d = 0; // 数组元素和的差，我们要让这个差变为 0
+        for (int x : nums2)
+            d += x;
+        for (int x : nums1)
+            d -= x;
+        if (d < 0) {
+            d = -d;
+            int[] tmp = nums1;
+            nums1 = nums2;
+            nums2 = tmp; // 交换，统一让 nums1 的数变大，nums2 的数变小
+        }
+        int[] cnt = new int[6]; // 统计每个数的最大变化量
+        for (int x : nums1) ++cnt[6 - x]; // nums1 的变成 6
+        for (int x : nums2) ++cnt[x - 1]; // nums2 的变成 1
+        for (int i = 5, ans = 0; ; --i) { // 从大到小枚举最大变化量 5 4 3 2 1
+            if (i * cnt[i] >= d) // 可以让 d 变为 0
+                return ans + (d + i - 1) / i;
+            ans += cnt[i]; // 需要所有最大变化量为 i 的数
+            d -= i * cnt[i];
+        }
+    }
+
+}
