1a2592714ab30ea48192441d7bc3de24cecea82f
yxl <1627419257@qq.com>
2022-12-21 20:47:30 +0800

    leetcode 2022.12.21

---
+++ src/yxl/demo/y2022/m12/Solution.java
@@ -0,0 +1,13 @@
+package yxl.demo.y2022.m12;
+
+/**
+ * @author yxl
+ * @date 2022/12/21 下午8:47
+ */
+public class Solution {
+    public int maximumScore(int a, int b, int c) {
+        int sum = a + b + c;
+        int maxVal = Math.max(Math.max(a, b), c);
+        return Math.min(sum - maxVal, sum / 2);
+    }
+}
