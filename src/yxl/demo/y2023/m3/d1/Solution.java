dfec1607b8db7cde9e5f9c70f2d37197a694dbc5
yxl <1627419257@qq.com>
2023-03-01 16:23:18 +0800

    leetcode 2023.3.1 new mouth

---
+++ src/yxl/demo/y2023/m3/d1/Solution.java
@@ -0,0 +1,22 @@
+package yxl.demo.y2023.m3.d1;
+
+/**
+ * @author yxl
+ * @date 2023/3/1 下午4:23
+ */
+public class Solution {
+    public int[][] largestLocal(int[][] grid) {
+        int n = grid.length;
+        int[][] res = new int[n - 2][n - 2];
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = 0; j < n - 2; j++) {
+                for (int x = i; x < i + 3; x++) {
+                    for (int y = j; y < j + 3; y++) {
+                        res[i][j] = Math.max(res[i][j], grid[x][y]);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
