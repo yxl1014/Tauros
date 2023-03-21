c53308690486726c1e026fb4e8a5f955da34d226
yxl <1627419257@qq.com>
2023-03-21 09:58:23 +0800

    leetcode 2023.3.20

---
+++ src/yxl/demo/y2023/m3/d21/Solution.java
@@ -0,0 +1,11 @@
+package yxl.demo.y2023.m3.d21;
+
+/**
+ * @author yxl
+ * @date 2023/3/21 上午9:57
+ */
+public class Solution {
+    public double[] convertTemperature(double celsius) {
+        return new double[]{celsius + 273.15, celsius * 1.80 + 32.00};
+    }
+}
