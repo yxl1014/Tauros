dd38f9bfa3d50fe843e7521746891f16a9966a51
yxl <1627419257@qq.com>
2023-03-27 13:58:41 +0800

    leetcode 2023.3.27

---
+++ src/yxl/demo/y2023/m3/d27/Solution.java
@@ -0,0 +1,27 @@
+package yxl.demo.y2023.m3.d27;
+
+/**
+ * @author yxl
+ * @date 2023/3/27 下午1:58
+ */
+public class Solution {
+    public int countSubstrings(String s, String t) {
+        int ans = 0;
+        int m = s.length(), n = t.length();
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (s.charAt(i) != t.charAt(j)) {
+                    int l = 0, r = 0;
+                    while (i - l > 0 && j - l > 0 && s.charAt(i - l - 1) == t.charAt(j - l - 1)) {
+                        ++l;
+                    }
+                    while (i + r + 1 < m && j + r + 1 < n && s.charAt(i + r + 1) == t.charAt(j + r + 1)) {
+                        ++r;
+                    }
+                    ans += (l + 1) * (r + 1);
+                }
+            }
+        }
+        return ans;
+    }
+}
